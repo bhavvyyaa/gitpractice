8def8edd1ef98573aebb9b0065a4c68da70ee5a3
bhavvyya <bhavyakrishnan2006@gmail.com>
2025-08-09 11:43:59 +0530

    Array Concatenation

---
+++ arrayConcat.py
@@ -0,0 +1,4 @@
+array_1 = list(map(int, input("Enter numbers separated by space: ").split()))
+array_2 = list(map(int, input("Enter numbers separated by space: ").split()))
+concatenatedArray=array_1+array_2
+print("the concatenated array is", concatenatedArray)
